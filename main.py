85bf95aba0cfa724a867be918a0f0184b6581e7d
geen <gqkuah2505@gmail.com>
2024-07-03 09:40:39 +0800

    modified:   main.py

--- main.py
+++ main.py
@@ -7,17 +7,12 @@ new_source = source.replace('self.jwt = None', ' ')
 exec(new_source, duolingo.__dict__)
 
 lingo  = duolingo.Duolingo('geengene', jwt='eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjYzMDcyMDAwMDAsImlhdCI6MCwic3ViIjoxNDI1NjI3MDAzfQ.9BF614NVShsb12Qafe9rFmqKa_wvAlyOQO_Z9qmCi88')
-known_words = lingo.get_known_words('ja')
+# known_words = lingo.get_known_words('ja')
 vocab = lingo.get_vocabulary(language_abbr='ja')
-# translation = lingo.get_translations(known_words, source='ja', target='en')
-for number, word in enumerate(known_words):
-    print(f"{number}: {word}")
+# for number, word in enumerate(known_words):
+#     print(f"{number}: {word}")
     
-for number, word in enumerate(vocab):
-    print(f"{number}: {word}")
-
-
-# for trans_word in translation:
-    #  print(trans_word)
+for number, word in enumerate(vocab): # word is a dictionary containing key value pairs
+    print(f"{number}: {word["text"]} - {word["translations"][0]}")
     
 
