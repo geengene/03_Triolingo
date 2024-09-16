1a0db20d73092db8115272d9fccd84f9001a1a7e
gene <gqkuah2505@gmail.com>
2024-09-16 13:52:19 +0800

    idk

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+import time
 import duolingo
 import inspect
 
@@ -8,11 +10,21 @@ exec(new_source, duolingo.__dict__)
 
 lingo  = duolingo.Duolingo('geengene', jwt='eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjYzMDcyMDAwMDAsImlhdCI6MCwic3ViIjoxNDI1NjI3MDAzfQ.9BF614NVShsb12Qafe9rFmqKa_wvAlyOQO_Z9qmCi88')
 # known_words = lingo.get_known_words('ja')
-vocab = lingo.get_vocabulary(language_abbr='ja')
+vocab = lingo.get_vocabulary(language_abbr='ja') # vocab is a list of dictionaries for each word
 # for number, word in enumerate(known_words):
 #     print(f"{number}: {word}")
-    
+
 for number, word in enumerate(vocab): # word is a dictionary containing key value pairs
-    print(f"{number}: {word["text"]} - {word["translations"][0]}")
+    # print(f"{number}: {word["text"]} - {word["translations"][0]}")
+    number = number
+    text = word["text"] 
+    translation = word["translations"]
+    print(number, text, translation)
+    
+
+
+
+
+    
     
 
